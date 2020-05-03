861aa63b1e24a6195a8369a3c18a1ce5ece29a9b
den4592 <50400631+den4592@users.noreply.github.com>
2020-05-03 17:31:50 +0900

    Add files via upload

---
+++ xl_to_data(AR).py
@@ -0,0 +1,25 @@
+import openpyxl
+import requests
+import json
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+wb = openpyxl.load_workbook("/Users/ilya/Desktop/link.xlsx")
+sheet = wb['Sheet1']
+column=1
+row=1
+count=0
+#print(sheet.cell(row=1, column=1).value)
+#print("%s" % sheet.max_column)
+col = sheet['A']               # 첫 번째 열 전체를 가져옴
+
+for cell in col:# 각 행에 대해서
+        html = urlopen('https://www.androidrank.org/'+str(sheet.cell(row,column).value))
+        bsObject = BeautifulSoup(html, "html.parser")
+        paragraph_data = bsObject.find_all('td', style='text-align:left;')
+        for a in paragraph_data:
+            print(a.find("a")["href"])
+            print('\n')
+            print(count)
+            count += 1
+        row +=3
